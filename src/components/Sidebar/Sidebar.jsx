a35768fd88e62e5592d5a15dd66db39a4b3821ee
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-03-20 22:09:41 +0200

    Added apply filters button

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import styles from './Sidebar.module.css'
 import { CONFIG } from '../../config'
@@ -7,9 +7,28 @@ function Sidebar() {
     const categories = CONFIG.categories
     const providers = CONFIG.providers
 
+    const [filtersVisibility, setFiltersVisibility] = useState(false)
+    const [width, setWindowWidth] = useState(0)
+    const [anySelected, setAnySelected] = useState(false)
+
+    useEffect(() => {
+        updateDimensions()
+
+        window.addEventListener('resize', updateDimensions)
+        return () => window.removeEventListener('resize', updateDimensions)
+    }, [])
+
+    const updateDimensions = () => setWindowWidth(window.innerWidth)
+
+    const select = () => setAnySelected(true)
+
     const renderAllCategories = () => {
         return categories.map(category => (
-            <div key={category.id} className={styles['category']}>
+            <div
+                key={category.id}
+                className={styles['category']}
+                onClick={select}
+            >
                 <img src={category.icon} alt="category-icon" />
                 <p>{category.displayName}</p>
             </div>
@@ -18,22 +37,72 @@ function Sidebar() {
 
     const renderAllProviders = () => {
         return providers.map(provider => (
-            <div key={provider.id} className={styles['provider']}>
+            <div
+                key={provider.id}
+                className={styles['provider']}
+                onClick={select}
+            >
                 <img src={provider.icon} alt="provider-icon" />
             </div>
         ))
     }
 
+    const handelFilterButton = () => {
+        return !filtersVisibility ? (
+            <button
+                className={styles['filters-closed']}
+                onClick={() => setFiltersVisibility(!filtersVisibility)}
+            >
+                <img src="./icons/settings.svg" alt="filters-icon" />
+                <span>Фильтры</span>
+            </button>
+        ) : (
+            <div className={styles['filters-opened']}>
+                <div>
+                    <span>Фильтры</span>
+                    <img
+                        src="./icons/cancel.svg"
+                        onClick={() => setFiltersVisibility(!filtersVisibility)}
+                        alt="cancel-icon"
+                    />
+                </div>
+
+                <hr />
+            </div>
+        )
+    }
+
+    const renderApplyFilterButton = () =>
+        width < 400 && anySelected ? (
+            <button className={styles['apply-filters-button']}>
+                Применить
+            </button>
+        ) : (
+            <> </>
+        )
+
     return (
         <div className={styles['container']}>
-            <div className={styles['categories']}>{renderAllCategories()}</div>
+            {width < 400 ? handelFilterButton() : <> </>}
 
-            <hr />
+            {(width < 400 && filtersVisibility) || width >= 400 ? (
+                <>
+                    <div className={styles['categories']}>
+                        {renderAllCategories()}
+                    </div>
 
-            <div className={styles['providers']}>
-                <h2>Провайдеры</h2>
-                {renderAllProviders()}
-            </div>
+                    <hr />
+
+                    <div className={styles['providers']}>
+                        <h2>Провайдеры</h2>
+                        {renderAllProviders()}
+                    </div>
+
+                    {renderApplyFilterButton()}
+                </>
+            ) : (
+                <></>
+            )}
         </div>
     )
 }

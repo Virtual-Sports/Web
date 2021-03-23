bce1057e09f21e9e1aa1a7e6661547c2596f5e66
Sergo <sergo@M-Air-3.netis>
2021-03-24 01:02:01 +0200

    changes by reviewers' comments

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import styles from './GameCard.module.css'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-const GameCard = ({ title, img }) => {
+const GameCard = ({ id, title, img }) => {
+    const defaultImageUrl = 'https://via.placeholder.com/250x250' // TODO: move to another file with constants
+    const defaultTitle = 'no title' // TODO: move to another file with constants
+
     return (
-        <div className={styles.card}>
-            <img src={img ?? 'https://via.placeholder.com/250x250'} />
-            <p className={styles.description}>{title ?? 'no title'}</p>
-        </div>
+        <Link className={styles.card} to={`/game/${id}`}>
+            <img src={img ?? defaultImageUrl} />
+            <p className={styles.description}>{title ?? defaultTitle}</p>
+        </Link>
     )
 }
 
 GameCard.propTypes = {
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     title: PropTypes.string.isRequired,
     img: PropTypes.string,
 }

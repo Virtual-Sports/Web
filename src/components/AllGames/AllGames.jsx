b9ddbbe3db6fdd7509f36a3296ec337d57861566
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-03-24 09:47:28 +0200

    Implemented the structure of game container

--- src/components/AllGames/AllGames.jsx
+++ src/components/AllGames/AllGames.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import styles from './AllGames.module.css'
 
+import { CONFIG } from '../../config'
+import GamesContainer from './GamesContainer/GamesContainer'
+
 function AllGames() {
-    return <div className={styles['container']}>Game page</div>
+    const tags = CONFIG.tags
+    const allGames = CONFIG.games
+
+    const [tagsGames, setTagsGames] = useState([])
+
+    useEffect(() => {
+        for (let tag of tags) {
+            const games = allGames.filter(game => game.tags.includes(tag.id))
+
+            if (games.length !== 0) {
+                setTagsGames(prev => [
+                    ...prev,
+                    {
+                        tag,
+                        icon: tag.icon ? tag.icon : null,
+                        games: allGames.filter(game =>
+                            game.tags.includes(tag.id)
+                        ),
+                    },
+                ])
+            }
+        }
+    }, [tags, allGames])
+
+    // eslint-disable-next-line no-console
+    console.log(tagsGames)
+
+    return (
+        <div className={styles['container']}>
+            <div className={styles['tags']}>
+                {tagsGames.map(item => (
+                    <div key={item.tag.id}>
+                        <GamesContainer
+                            key={item.tag.id}
+                            title={item.tag.name}
+                            icon={item.icon}
+                            games={item.games}
+                        />
+
+                        <GamesContainer
+                            key={item.tag.id}
+                            title={item.tag.name}
+                            icon={item.icon}
+                            games={item.games}
+                        />
+
+                        <GamesContainer
+                            key={item.tag.id}
+                            title={item.tag.name}
+                            icon={item.icon}
+                            games={item.games}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default AllGames
--- src/components/AllGames/GamesContainer/GamesContainer.jsx
+++ src/components/AllGames/GamesContainer/GamesContainer.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function GamesContainer() {
-    return <div>Game container</div>
+import styles from './GamesContainer.module.css'
+
+import GameCard from '../GameCard/GameCard'
+
+GamesContainer.propTypes = {
+    icon: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    games: PropTypes.array.isRequired,
+}
+
+function GamesContainer({ icon = null, title, games }) {
+    return (
+        <div className={styles['container']}>
+            <div className={styles['header']}>
+                {icon ? <img src={icon} alt="icon" /> : <></>}
+                <h2 className={styles['title']}>{title}</h2>
+            </div>
+
+            <div className={styles['games']}>
+                {games.map(game => (
+                    <GameCard key={game.id} game={game} />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default GamesContainer

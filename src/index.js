312d6d11be9a256b862fb723c13af664871cd9f9
Volodymyr Buts <52294099+VolodymyrButs@users.noreply.github.com>
2021-03-29 13:05:24 +0300

    PWA, little fix (#34)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import './reset.css'
 import './index.css'
-
 import App from './App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
+import './reset.css'
 import { Provider } from 'react-redux'
-
 import store from './redux/store'
 
 ReactDOM.render(
@@ -18,6 +16,11 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
